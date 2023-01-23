63ece18d8dc1b97b53f118495ece9529449b0a91
Ste-Mich <72508715+Ste-Mich@users.noreply.github.com>
2023-01-24 00:20:22 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+/*  Prototype (clone) se používá pro ziskání instance se stejným stavem.
+ *  - Místo ručního kopírování třída implementuje Clone metodu, která vrací kopii instance
+ *  - Není problém s private vlastnostmi
+ */
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new();
+            dog.Name = "Original Dog";
+            dog.MakeSound();
+            dog.MakeNewHideout();
+            dog.MakeNewHideout();
+            dog.PrintHideoutNumber();
+            dog.PrintAge();
+
+            Dog dogCopy = dog.Clone();
+            dogCopy.Name = "Clone dog";
+            dogCopy.MakeSound();
+            dogCopy.MakeNewHideout();
+            dogCopy.MakeNewHideout();
+            dogCopy.PrintHideoutNumber();
+            dogCopy.PrintAge();
+        }
+    }
+
+    public interface ICloning<T>
+    {
+        public T Clone();
+    }
+
+    public class Dog : ICloning<Dog> //ICloneable
+    {
+        private int AgeInYears { get; set; }
+        private int NumberOfHideouts { get; set; }
+        public string Name { get; set; }
+        public bool IsCute { get; set; }
+
+        public void MakeSound()
+        {
+            Console.WriteLine(Name + " woofs!");
+        }
+
+        public void MakeNewHideout()
+        {
+            NumberOfHideouts++;
+        }
+
+        public void PrintHideoutNumber()
+        {
+            Console.WriteLine(Name + " has " + NumberOfHideouts + " hideouts.");
+        }
+
+        public void PrintAge()
+        {
+            if (AgeInYears > 5)
+            {
+                Console.WriteLine("Iam old.");
+            }
+            else
+            {
+                Console.WriteLine("Iam young.");
+            }
+        }
+
+        public Dog Clone()
+        {
+            Dog dogCopy = new();
+            dogCopy.AgeInYears = this.AgeInYears;
+            dogCopy.NumberOfHideouts = this.NumberOfHideouts;
+            dogCopy.Name = this.Name;
+            dogCopy.IsCute = this.IsCute;
+            return dogCopy;
+        }
+    }
+}
